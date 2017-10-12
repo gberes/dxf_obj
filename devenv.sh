ed73f95d3feb390adda02d9ac0407a811fc0ce1c
Richard Thier <u9vata@gmail.com>
2017-10-12 19:32:45 +0200

    Added ctags file for this small project

--- devenv.sh
+++ devenv.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
+# TODO: Only run ctags when there if some file is untouched maybe...
+# Run ctags so we can use ctrl+] to jump for definitions and stuff
+ctags -R -f ./tags .
+# Open all the stuff in vim - prepared nicely
 vim -p makefile *.cpp *.h
