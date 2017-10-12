8f94b2cc0cf56ffe46f39ecc3288b06d9a889b50
Richard Thier <u9vata@gmail.com>
2017-10-12 19:51:16 +0200

    Updated devenv.sh so that it generates ctags only daily - if you need more delete the file!

--- devenv.sh
+++ devenv.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 
-# TODO: Only run ctags when there if some file is untouched maybe...
-# Run ctags so we can use ctrl+] to jump for definitions and stuff
-ctags -R -f ./tags .
+# To only run ctags if the tags file is at least 1 days old...
+# ...collect both times in seconds-since-the-epoch...
+filename=tags
+one_day_ago=$(date -d 'now - 1 days' +%s)
+file_time=$(date -r "$filename" +%s)
+
+# ...and then just use integer math:
+if (( file_time <= hundred_days_ago )); then
+  echo "$filename is older than 1 day, generating ctags please be patient..."
+  # Run ctags so we can use ctrl+] to jump for definitions and stuff
+  ctags -R -f ./"$filename" .
+  echo "...ctags done!"
+fi
 # Open all the stuff in vim - prepared nicely
 vim -p makefile *.cpp *.h
